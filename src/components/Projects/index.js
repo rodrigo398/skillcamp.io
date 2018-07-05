14d7024742c5bc49bed06c851f4e94f2e3169e1f
mixolydian251 <tjhmusic11@gmail.com>
2018-07-04 23:50:47 -0400

    feature: project section

---
+++ src/components/Projects/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ProjectsWrapper = styled.div`
+  background: #222222;
+  border-left: 10px solid green;
+  border-right: 10px solid green;
+`
+
+const Projects = () => {
+  return <ProjectsWrapper />
+}
+
+export default Projects
