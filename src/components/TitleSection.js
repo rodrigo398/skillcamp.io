2bc171e7f4041cb0c9fa3a2a45890f3ae4611022
Sammy Munoz <sdmunoz@yahoo.com>
2018-07-08 17:23:03 -0500

    Feature: title section fixes 2

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -4,33 +4,35 @@ import developer from '../images/developer_blur.jpg'
 
 const TitleSectionWrapper = styled.div`
   background: url(${developer});
-  padding: 100px 20px 300px 20px;
   background-size: cover;
+  height: 100vh;
   display: grid;
+  justify-items: left;
   grid-template-columns: repeat(2, 1fr);
-  grid-template-areas: ' .   title';
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-areas:
+    ' . . '
+    ' . title '
+    ' . . ';
   @media screen and (max-width: 600px) {
-    padding: 20px;
     grid-template-columns: repeat(1, 1fr);
   }
 `
 
 const TitleSection = styled.div`
   grid-area: title;
+  padding: 10px;
 `
 
 const Title = styled.div`
-  margin-right: 50px;
   background-color: rgba(0, 0, 0, 0.5);
-  width: 400px;
   border-radius: 10px;
-  padding: 40px 30px;
   font-size: 60px;
   color: #ffffff;
-  margin-bottom: 20px;
   text-align: left;
+  padding: 40px 30px;
+  margin-bottom: 10px;
   @media screen and (max-width: 600px) {
-    padding: 40px 20px;
     font-size: 40px;
     width: 100%;
     text-align: center;
@@ -38,16 +40,14 @@ const Title = styled.div`
 `
 
 const TitleContent = styled.div`
-  margin-right: 50px;
   background-color: rgba(0, 0, 0, 0.5);
   width: 400px;
   border-radius: 10px;
-  padding: 20px 20px;
   font-size: 20px;
   color: #ffffff;
   text-align: left;
+  padding: 20px;
   @media screen and (max-width: 600px) {
-    padding: 20px;
     font-size: 20px;
     width: 100%;
     text-align: center;
